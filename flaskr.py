5e19aac0132137c9ecdf04641bc4503714268df0
george-campos <george.campos@gmail.com>
2021-06-18 17:36:16 +0000

    mensagem

---
+++ flaskr.py
@@ -0,0 +1,29 @@
+import sqlite3
+from flask import Flask, g
+
+#configurações
+
+DATABASE = './flaskr.db'
+SECRET_KEY = "pudim"
+USER = "admin"
+PASSWORD = "senha"
+
+#APLICACAO
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+def connect_db():
+    return sqlite3.connect(DATABASE)
+
+@app.before_first_request
+def before():
+    g.db = connect_db()
+
+@app.teardown_request
+def after(exception):
+    g.db.close()
+
+@app.route('/')
+def index():
+    return "<h1>Hello World, George</h1>"
